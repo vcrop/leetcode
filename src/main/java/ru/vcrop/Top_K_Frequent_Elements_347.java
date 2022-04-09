818b786dd5abf7227816c6490b0e9043d657101d
vcrop <viktor.raguev@gmail.com>
2022-04-09 10:52:06 +0300

    add 347

---
+++ src/main/java/ru/vcrop/Top_K_Frequent_Elements_347.java
@@ -0,0 +1,18 @@
+package ru.vcrop;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/*Runtime: 18 ms, faster than 46.66% of Java online submissions for Top K Frequent Elements.
+        Memory Usage: 45 MB, less than 85.42% of Java online submissions for Top K Frequent Elements.*/
+public class Top_K_Frequent_Elements_347 {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        return IntStream.of(nums).boxed().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(k).mapToInt(l -> (int) l.getKey()).toArray();
+    }
+}
