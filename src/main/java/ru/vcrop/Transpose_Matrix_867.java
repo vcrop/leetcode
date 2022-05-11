1b12b84d8d7a6d73158b6b898b1fbaec3288be02
vcrop <viktor.raguev@gmail.com>
2022-05-11 15:39:17 +0300

    867

--- src/main/java/ru/vcrop/Transpose_Matrix_867.java
+++ src/main/java/ru/vcrop/Transpose_Matrix_867.java
@@ -1,2 +1,16 @@
-package ru.vcrop;public class Transpose_Matrix_867 {
+package ru.vcrop;
+
+/*Runtime: 0 ms, faster than 100.00% of Java online submissions for Transpose Matrix.
+        Memory Usage: 43 MB, less than 85.59% of Java online submissions for Transpose Matrix.*/
+public class Transpose_Matrix_867 {
+
+    public int[][] transpose(int[][] matrix) {
+        int[][] ret = new int[matrix[0].length][matrix.length];
+
+        for (int row = 0; row < matrix.length; row++)
+            for (int col = 0; col < matrix[0].length; col++)
+                ret[col][row] = matrix[row][col];
+
+        return ret;
+    }
 }
