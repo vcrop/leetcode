b14870c4e242713a98f2c5d313ac90f04415924a
vcrop <viktor.raguev@gmail.com>
2022-06-22 13:48:07 +0300

    75-876

---
+++ src/main/java/ru/vcrop/Middle_of_the_Linked_List_876.java
@@ -0,0 +1,25 @@
+package ru.vcrop;
+
+public class Middle_of_the_Linked_List_876 {
+
+      public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast.next == null) return slow;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+}
